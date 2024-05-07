eceaaaa2989bcd36b013dce2a9b5a13f5f914926
Madison Moulder <madmogalaxy89@att.net>
2024-05-07 14:00:31 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
+ const [puppies, setPuppies] = useState(puppyList)
  
-
-  return 
-    
-      <div>
-      
-      </div>
-
+  console.log("puppyList: ", puppyList);
+ 
+  return(
+  
+  <div className="App">
+  {
+    puppies.map((puppy) => {
+      return <p key={puppy.id}>{puppy.name}</p>
+    })
+  }
+</div>
+  );
 }
 
-export default App
+   
+
+export default App;
